e1ee2e71452316daca560e35b40b6fbd81bad62f
rizkidarmawan2468 <mochamadrizkidarmawan4@gmail.com>
2025-03-09 11:56:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Anda dapat menambahkan interaksi JavaScript di sini jika diperlukan
+// Misalnya, mengganti gambar profil secara dinamis
+document.getElementById("profileImage").addEventListener("click", function () {
+  this.src = "https://via.placeholder.com/150/FF0000/FFFFFF?text=Gambar+Baru";
+});
